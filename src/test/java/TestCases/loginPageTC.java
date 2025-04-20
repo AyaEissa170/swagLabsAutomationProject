e53ded20968ec2cf8a82847976813207cf095cc1
ayaeissa <ayaeissa170@gmail.com>
2025-04-20 16:23:47 +0200

    added allure setup and listener,fixed config

--- src/test/java/TestCases/loginPageTC.java
+++ src/test/java/TestCases/loginPageTC.java
@@ -4,6 +4,7 @@ import Pages.P01_LoginPage;
 import Pages.P02_HomePage;
 import Pages.P03_Cart;
 import Pages.P04_Checkout;
+import io.qameta.allure.testng.AllureTestNg;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
@@ -15,26 +16,18 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import java.time.Duration;
 
+//@Listeners({AllureTestNg.class})
 public class loginPageTC {
     private WebDriver driver;
     P01_LoginPage loginPage;
     P02_HomePage selectProduct;
     P03_Cart Cart;
     P04_Checkout Checkout;
-public Logger LOGGER= LogManager.getLogger();
+
     @BeforeMethod ( alwaysRun = true)
     public void setup() {
-        /*
-        Logger logger;
-        logger=Logger.getLogger(setup.class.getName());
-        logger.setLevel(Level.INFO);// we will get a clear view of the test flow and important events that occur during the test execution.
-        logger.info("Running test in " + browser);
-        */
 
         driver = new EdgeDriver();
         driver.manage().window().maximize();
@@ -42,9 +35,9 @@ public Logger LOGGER= LogManager.getLogger();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
-    @Test (priority = 1, groups={"regression"}
+    @Test (priority = 1, groups={"regression"})
     public void HardValidationTc() {
-        LOGGER.info("Test case is started");
+
         P01_LoginPage loginPage= new P01_LoginPage(driver);
         loginPage.enterUserName("error_user");
         loginPage.enterPassword("secret_sauce");
